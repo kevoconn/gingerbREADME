4c1cbdee89d2cfdce72af97ef5b53e8ed55e52e5
github_kevoconn <kcoconnor84@gmail.com>
2023-01-09 12:12:32 -0600

    adjust questions, build readme outline

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
-# l;
-  ## Badges
-  <img src="https://img.shields.io/badge/license-Academic Free License v3.0-blue.svg">
-  This project is licensed under $[license]
+# nikmn
+  <img src="https://img.shields.io/badge/license-GitLab-blue.svg">
+   # Description 
+  knhi
+  # Table of Contents 
+  * [Installation](#install)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#testing)
+  * [Questions](#questions)
+  # Installation 
+  undefined
+  # Usage 
+  njnju
+  # License
+This project is licensed under the GitLab license
+  # Contributing 
+  jijbu
+  # Tests 
+  undefined
+  # Questions 
+  For further information:
+ 
+  * GitHub:
+    * [https://github.com/ininuhb](https://github.com/ininuhb)
+ 
+  * Email:
+    * ijni
 
 
   
--- index.js
+++ index.js
@@ -6,61 +6,28 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-  { type: "title", message: "what is the title of this application?", name: "title" },
+  { type: "input", message: "What is the title of this application?", name: "title" },
 
-  { type: "input", name: "description", message: "What does this application do?" },
+  { type: "input", message: "What does this application do?", name: "description" },
 
-  { type: "input", name: "install", message: "How do I install this application?" },
+  { type: "input", message: "How do I install this application?", name: "install" },
 
-  { type: "input", name: "usage", message: "How do I use this application?" },
+  { type: "input", message: "How do I use this application?", name: "usage" },
 
   {
-    name: "license",
     type: "list",
     message: "Which license you would like to use for this application?",
-    choices: [
-      "Academic Free License v3.0",
-      "Apache license 2.0",
-      "Artistic license 2.0",
-      "Boost Software License 1.0",
-      "BSD 2-clause license",
-      "BSD 3-clause license",
-      "Creative Commons Zero v1.0 Universal",
-      "Creative Commons Attribution 4.0",
-      "Creative Commons Attribution Share Alike 4.0",
-      "Do What The Fuck You Want To Public License",
-      "Educational Community License v2.0",
-      "Eclipse Public License 1.0",
-      "Eclipse Public License 2.0",
-      "European Union Public License 1.1",
-      "GNU Affero General Public License v3.0",
-      "GNU General Public License v2.0",
-      "GNU General Public License v3.0",
-      "GNU Lesser General Public License v2.1",
-      "GNU Lesser General Public License v3.0",
-      "ISC",
-      "LaTeX Project Public License v1.3c",
-      "Microsoft Public License",
-      "MIT",
-      "Mozilla Public License 2.0",
-      "none",
-      "Open Software License 3.0",
-      "SIL Open Font License 1.1",
-      "University of Illinois/NCSA Open Source License",
-      "The Unlicense",
-      "zLib License",
-    ],
+    choices: ["none", "MIT", "Bower", "NPM", "GitLab"],
+    name: "license",
   },
 
-  { type: "confirm", name: "tableOfContents", message: "Do you want a Table of Contents?"},
+  { type: "input", message: "How do I contribute to this application?", name: "contributing" },
 
-  { type: "input", name: "contributing", message: "How do I contribute to this application?" },
+  { type: "input", message: "How do I test this application?", name: "testing" },
 
-  { type: "input", name: "testing", message: "How do I test this application?" },
+  { type: "input", message: "What is your Github username?", name: "username" },
 
-  { type: "input", name: "username", message: "What is your Github username?" },
-
-  { type: "input", name: "email", message: "What is your e-mail?" },
+  { type: "input", message: "What is your e-mail?", name: "email" },
 ];
 
 // TODO: Create a function to write README file
