52bce9c215cc38a51d883f4219bf57a6b49b8769
github_kevoconn <kcoconnor84@gmail.com>
2023-01-09 11:47:53 -0600

    generateMarkdown functions

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# f;
+# l;
   ## Badges
   <img src="https://img.shields.io/badge/license-Academic Free License v3.0-blue.svg">
+  This project is licensed under $[license]
 
 
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,17 +10,31 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "none"){
+  return `* [License](#license)`;
+  } else {
+    return "";
+  }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "none") {
+    return `# License\nThis project is licensed under the ${license} license`}
+    else{
+      return "";
+    }
+  }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title};
   ## Badges
   ${renderLicenseBadge(data.license)}
+  ${renderLicenseSection(data.license)}
 
 
   
